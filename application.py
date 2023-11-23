63ba305310fe79a8ce0469cdf0d233a6e7d3c70f
Sarang <hangpfm0518@ewhain.net>
2023-11-23 21:46:55 +0900

    1. 리뷰작성 화면 구현

--- application.py
+++ application.py
@@ -47,9 +47,15 @@ def view_review():
 def reg_item():
     return render_template("reg_items.html")
 
-@application.route("/reg_reviews")
+@application.route("/reg_review_init/<name>/") 
+def reg_review_init(name):
+    return render_template("reg_reviews.html", name=name)
+
+@application.route("/reg_review", methods=['POST']) 
 def reg_review():
-    return render_template("reg_reviews.html")
+    data=request.form
+    DB.reg_review(data)
+    return redirect(url_for('view_review'))
 
 @application.route("/submit_item")
 def reg_item_submit():
--- database.py
+++ database.py
@@ -78,3 +78,11 @@ class DBhandler:
             if key_value == name:
                 target_value=res.val()
         return target_value
+    
+    def reg_review(self, data):
+        review_info ={
+            "rate": data['reviewStar'],
+            "review": data['reviewContents']
+        }
+        self.db.child("review").child(data['name']).set(review_info)
+        return True
